7484ac01d52c8a41df7e359f857bea4c9c3e2f06
akroms <Akromiphone7.90@gmail.com>
2021-09-27 20:01:13 +0500

    javascript

--- js/script.js
+++ js/script.js
@@ -1,20 +1,22 @@
-let btn = document.getElementById("btn");
-let randomNumber = Math.floor(Math.random() * 10); 
-
-let xhr = new XMLHttpRequest(); 
+let quoteContainer = document.querySelector(".quote"),
+    randomNumber = Math.floor(Math.random() * 10),
+    xhr = new XMLHttpRequest();
 
 xhr.open("GET", "https://quote-garden.herokuapp.com/api/v3/quotes", true);
+const loadAPI = () => {
+    let responseString = xhr.responseText,
+        responseObject = JSON.parse(responseString),
+        randomQuote = responseObject.data[randomNumber],
+        quoteText = randomQuote.quoteText,
+        quoteAuthor = randomQuote.quoteAuthor;
 
-let loadAPI = () => {
-    let responseString = xhr.responseText;
-    let responseObject = JSON.parse(responseString); 
-    console.log(responseObject.data[randomNumber]);
-}
-
-xhr.addEventListener("load", loadAPI); 
-btn.addEventListener("click", loadAPI);
+    /* Quote in HTML */
+    quoteContainer.innerHTML = `
+            <p class="quote-text">${quoteText}</p>
+            <p class="quote-author">${quoteAuthor}</p>
+        `;
+    console.log(randomNumber);
+};
 
 xhr.send();
-
-
- 
+xhr.addEventListener("load", loadAPI);
